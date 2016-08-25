9adc39883535bb4fd342966bb7eacbe859a806a4
985072 <985072@CSLAB-V29-1-1.cs.mum.edu>
2016-08-25 17:21:33 -0500

    Project Name refactored

--- OlineOrderingSystem/src/main/java/com/cuisine_mart/OnlineOrderingSystemApplication.java
+++ OlineOrderingSystem/src/main/java/com/cuisine_mart/OnlineOrderingSystemApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OlineOrderingSystemApplication {
+public class OnlineOrderingSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OlineOrderingSystemApplication.class, args);
+		SpringApplication.run(OnlineOrderingSystemApplication.class, args);
 	}
 }
--- OlineOrderingSystem/src/test/java/com/cuisine_mart/OnlineOrderingSystemApplicationTests.java
+++ OlineOrderingSystem/src/test/java/com/cuisine_mart/OnlineOrderingSystemApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class OlineOrderingSystemApplicationTests {
+public class OnlineOrderingSystemApplicationTests {
 
 	@Test
 	public void contextLoads() {
