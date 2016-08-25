c32a9ac7765953a1d2141537a5df16e9b066d097
Rajiv Neupane <htdrajiv@gmail.com>
2016-08-25 18:11:51 -0500

    added initial project structure

---
+++ OlineOrderingSystem/src/main/java/com/cuisine_mart/restaurant/controller/Test.java
@@ -0,0 +1,17 @@
+package com.cuisine_mart.restaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Rajiv on 8/25/2016.
+ */
+@Controller
+public class Test {
+    @RequestMapping("/testUrl")
+    public String testView(ModelMap modelMap){
+        modelMap.addAttribute("message","this is test message");
+        return "demo";
+    }
+}
---
+++ OlineOrderingSystem/src/main/java/com/cuisine_mart/restaurant/domain/Test.java
@@ -0,0 +1,7 @@
+package com.cuisine_mart.restaurant.domain;
+
+/**
+ * Created by Rajiv on 8/25/2016.
+ */
+public class Test {
+}
