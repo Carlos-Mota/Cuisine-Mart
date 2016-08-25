13f51a16029618b62cba869feeb8ecda3bcab73b
985072 <985072@CSLAB-V29-1-1.cs.mum.edu>
2016-08-25 16:20:21 -0500

    Starter

---
+++ OlineOrderingSystem/src/main/java/com/cuisine_mart/OlineOrderingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.cuisine_mart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlineOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlineOrderingSystemApplication.class, args);
+	}
+}
