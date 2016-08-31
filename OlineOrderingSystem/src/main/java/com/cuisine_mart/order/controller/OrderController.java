120dec57d23a59bbb8dacd7e2b0c4e6cc70fc71d
Eyuel Taddese <eyuel.taddese4@gmail.com>
2016-08-31 00:56:34 -0500

    OrderAddress

--- OlineOrderingSystem/src/main/java/com/cuisine_mart/order/controller/OrderController.java
+++ OlineOrderingSystem/src/main/java/com/cuisine_mart/order/controller/OrderController.java
@@ -1,8 +1,16 @@
 package com.cuisine_mart.order.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value="/order")
 public class OrderController {
+	@RequestMapping(value="/address",method= RequestMethod.GET)
+	public String orderAddressPage(Model model) {
+		return "orderAddress";
+	}
 
 }
